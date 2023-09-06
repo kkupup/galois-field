d892659e61f17aa5e3458b33d13384de98c69a96
Gru-kkupup <38217950+kkupup@users.noreply.github.com>
2023-09-06 10:34:58 +0800

    Update GaloisField.py

--- GaloisField.py
+++ GaloisField.py
@@ -176,10 +176,6 @@ class GF:
 
 
 if __name__ == '__main__':
-    gf = GF(3)
-    Mat1 = np.array([[0, 3, 2, 5], [0, 1, 3, 4], [4, 2, 3, 1], [4, 3, 3, 4]])
-    # a = np.array([[1, 2, 3, 4],[2, 3, 4, 5],[3, 4, 5, 6],[4, 5, 6, 7]])
-    b = gf.inverse_matrix(Mat1)
-    print(gf.matrix_multiplication(Mat1, b))
+    gf = GF(8)
 
 
